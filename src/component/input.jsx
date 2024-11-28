ba2e32ae90259e92b06e1acbbe197d47c1297bb4
Zenn-ith <adrianreynaldi7546@gmail.com>
2024-11-28 22:33:06 +0700

    input.jsx

--- src/component/input.jsx
+++ src/component/input.jsx
@@ -1,19 +1,28 @@
-function Input (){
-    const  logika = () => {
-       const nama = document.querySelector('input').value
-       document.querySelector('#nama').innerText = nama;
-      }
-      
-  return(
+import React, { useState } from 'react';
+
+const Input = () => {
+  const [nama, setNama] = useState('');
+
+  const handleInputChange = (e) => {
+    setNama(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    document.getElementById('nama').innerText = nama;
+  };
+
+  return (
     <div className="inputContainer">
-       <p id="nama"></p>
- 
-     <input type="text" placeholder="Ian"/>
-     <button onClick={logika}    
-      >Sumbit</button>
- 
+      <p id="nama"></p>
+      <input 
+        type="text" 
+        placeholder="Masukkan nama Anda" 
+        value={nama} 
+        onChange={handleInputChange} 
+      />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
-  )
- }
- 
- export default Input
+  );
+};
+
+export default Input;
